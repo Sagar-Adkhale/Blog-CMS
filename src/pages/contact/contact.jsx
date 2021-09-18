fac225db5483f6298f617b8ddc0a08b2ede03f33
sagar adkhale <sagar@workplacefabric.com>
2021-09-18 09:51:37 +0530

    contact from done

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,9 +1,50 @@
-
+import React, { useRef, useState } from 'react';
+import emailjs from 'emailjs-com';
 import "./contact.css";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import contactlogo from '../../asset/contact.png'
+import contactlogo from '../../asset/contact.png';
+import Alert from 'react-bootstrap/Alert'
 export default function Contact() {
- 
+  const form = useRef();
+  const [show, setShow] = useState(false);
+
+  function AlertDismissible() {
+  
+    return (
+      <>
+        <Alert show={show} variant="success">
+          <Alert.Heading><b>Thanks for being awesome!</b></Alert.Heading>
+          <p>
+          We have received your message and would like to thank you for writing to us. If your inquiry is urgent, please use the telephone number listed below to talk to one of our staff members. 
+          </p>
+          <p>
+          Otherwise, we will reply by email as soon as possible.
+          </p>
+          <hr />
+          <p><b>Talk to you soon, GUTS</b></p>
+        </Alert>
+  
+      </>
+    );
+  }
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log(form.current);
+    console.log(process.env.REACT_APP_YOUR_SERVICE_ID);
+
+    emailjs.sendForm(process.env.REACT_APP_YOUR_SERVICE_ID, process.env.REACT_APP_YOUR_TEMPLATE_ID, form.current, process.env.REACT_APP_YOUR_USER_ID)
+      .then((result) => {
+       
+        setShow(true);
+        window.setTimeout(()=>{
+          setShow(false);
+        },5000)
+          console.log("result",result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
       <div>
     <div className="contact3 py-5">
@@ -18,11 +59,11 @@ export default function Contact() {
         <div className="col-lg-6">
           <div className="contact-box ml-3">
             <h1 className="font-weight-light mt-2">Quick Contact</h1>
-            <form className="mt-4">
+            <form ref={form} onSubmit={sendEmail}>
               <div className="row">
                 <div className="col-lg-12">
                   <div className="form-group mt-2">
-                    <input className="form-control" type="text" placeholder="name"/>
+                    <input className="form-control" type="text" name="name" placeholder="name"/>
                   </div>
                 </div>
                 <div className="col-lg-12">
@@ -41,11 +82,13 @@ export default function Contact() {
                   </div>
                 </div>
                 <div className="col-lg-12">
-                  <button type="submit" className="btn btn-danger-gradiant mt-3 text-white border-0 px-3 py-2"><span> SUBMIT</span></button>
+                  <button type="submit" className="btn btn-danger-gradiant mt-3 text-white border-0 px-3 py-2" value="Send" ><span> SUBMIT</span></button>
                 </div>
               </div>
             </form>
           </div>
+          <AlertDismissible />
+
         </div>
         <div className="col-lg-12">
           <div className="card mt-4 border-0 mb-4">
