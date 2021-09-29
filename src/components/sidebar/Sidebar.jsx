29796c8847d81a464c537aa2144186a6405c9bd3
sagar adkhale <sagar@workplacefabric.com>
2021-09-29 16:12:31 +0530

    change about us and enable social

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,7 +11,8 @@ export default function Sidebar() {
           alt=""
         />
         <div className="sidebarSlogan">
-        There are many bike sayings about bike popularity and biking, but one of my favorites saying about biking is “Learn to ride a bike. You will not regret it if you live.”  
+        Guts is bikepacking solution which intends to add to the culture of bike touring in Country. Our philosophy is very much aligned to the quote by Ernest Hemingway “It is by riding a bicycle that you learn the contours of a country best, since you have to sweat up the hills and coast down them.”
+        {/* There are many bike sayings about bike popularity and biking, but one of my favorites saying about biking is “Learn to ride a bike. You will not regret it if you live.”   */}
         </div>
       </div>
       {/* {categories && categories.length>0 ?       <div className="sidebarItem">
@@ -27,10 +28,14 @@ export default function Sidebar() {
      <div className="sidebarItem">
         <span className="sidebarTitle">FOLLOW US</span>
         <div className="sidebarSocial">
-          <i className="sidebarIcon fab fa-facebook-square"></i>
-          <i className="sidebarIcon fab fa-twitter-square"></i>
-          <i className="sidebarIcon fab fa-pinterest-square"></i>
-          <i className="sidebarIcon fab fa-instagram-square"></i>
+          {/* <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i> */}
+          <a className="a" target="_blank" rel="noreferrer" href="https://wa.me/message/BYB5BY7ZYBV4C1">
+          <i className="sidebarIcon fab fa-whatsapp-square fa-lg"></i>
+          </a>
+          <a className="a" target="_blank" rel="noreferrer" href="https://www.instagram.com/invites/contact/?i=my5v89ybqxw9&utm_content=kdf3qyz">
+          <i className="sidebarIcon fab fa-instagram-square fa-lg"></i>
+          </a>
         </div>
       </div>
     </div>
