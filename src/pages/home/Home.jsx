c865f2527a924d1abb1f00c095fd55a0bc0b1300
sagar adkhale <sagar@workplacefabric.com>
2021-10-01 10:11:55 +0530

    home changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,31 +1,28 @@
-import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Slider from "../../components/slider/Slider";
 import "./home.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function Home() {
-  
   return (
     <>
-        <div id="container">
-
-      <Header />
-      {/* <div className="home container"> */}
-      <div className="contact3 py-5">
-  <div className="row no-gutters">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-9">
-        <Posts  />
+      <div id="container">
+        <Slider />
+        <div className="contact3 py-5">
+          <div className="row no-gutters">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-9">
+                  <Posts />
+                </div>
+                <div className="col-lg-3">
+                  <Sidebar />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="col-lg-3">
-
-        <Sidebar />
-        </div>
-        </div>
-        </div>
-        </div>
-      </div>
       </div>
     </>
   );
