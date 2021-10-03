876c0803834656134811e31a34fff33a21f9d484
sagar adkhale <sagar@workplacefabric.com>
2021-10-03 18:44:18 +0530

    slider time

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -10,7 +10,7 @@ export default function Slider() {
       <Carousel
         autoPlay="true"
         infiniteLoop={true}
-        interval={2000}
+        interval={2500}
         showThumbs={false}
         showStatus={false}
       >
